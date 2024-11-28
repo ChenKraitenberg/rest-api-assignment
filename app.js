056247e62a06301fbdbd80b992adc3618bab0e7a
Chen <Chenkrai@gmail.com>
2024-11-28 21:35:11 +0200

    update setup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const postRoutes = require("./routes/postRoutes");
+const postRoutes = require("./routes/postsRoutes");
 //const commentRoutes = require("./routes/commentRoutes");
 
 const app = express();
