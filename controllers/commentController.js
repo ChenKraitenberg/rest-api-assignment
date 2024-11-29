fe36fb8e576307bfaaf4564dc592081054518bfe
Chen <Chenkrai@gmail.com>
2024-11-29 15:35:06 +0200

    Added get all comments feature

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,26 +1,36 @@
-const Comments = require('../models/comment_models');
+const Comments = require("../models/comment_models");
 
 const addComment = async (req, res) => {
-    const { content, author, postId } = req.body;
+  const { content, author, postId } = req.body;
 
-    // אימות נתונים
-    if (!content || !author || !postId) {
-        return res.status(400).send("All fields (content, author, postId) are required.");
-    }
+  // אימות נתונים
+  if (!content || !author || !postId) {
+    return res
+      .status(400)
+      .send("All fields (content, author, postId) are required.");
+  }
 
-    console.log(req.body);
+  console.log(req.body);
 
-    try {
-        // יצירת התגובה, אין צורך לספק commentId כי הוא יווצר אוטומטית
-        const comment = await Comments.create({ content, author, postId });
-        res.status(201).send(comment);
-    } catch (err) {
-        res.status(400).send(err.message);
-    }
+  try {
+    // יצירת התגובה, אין צורך לספק commentId כי הוא יווצר אוטומטית
+    const comment = await Comments.create({ content, author, postId });
+    res.status(201).send(comment);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
 };
 
+const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comments.find();
+    res.status(200).send(comments);
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+};
 
 module.exports = {
-    addComment
-  };
-  
+  addComment,
+  getAllComments,
+};
